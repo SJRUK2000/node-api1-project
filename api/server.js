4054f253a6a06f9be30ddd6fd2193fbc8f47b07a
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-05-16 17:53:33 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,52 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+
+server.use(express.json())
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const posUser = await User.findById(req.params.id)
+    if (!posUser) {
+        res.status(404).json({ 
+            message: "The user with the specified ID does not exist" 
+        })
+    } else {
+        const deletedUser = await User.remove(posUser.id)
+        res.status(200).json(deletedUser)
+    }  
+    } catch (err) {
+        res.status(500).json({
+            message:'error creating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user" 
+        })
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch( err => {
+        res.status(500).json({
+            message:'error creating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
